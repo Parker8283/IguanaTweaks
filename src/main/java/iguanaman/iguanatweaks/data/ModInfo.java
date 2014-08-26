cde880a93867f4e11b16044a6ce25f0cc814a449
Parker <pyoung1998@gmail.com>
2014-08-26 14:17:01 -0400

    Force build 1185. Closes #19.

--- src/main/java/iguanaman/iguanatweaks/data/ModInfo.java
+++ src/main/java/iguanaman/iguanatweaks/data/ModInfo.java
@@ -4,6 +4,6 @@ public abstract class ModInfo {
     public static final String MODID = "IguanaTweaks";
     public static final String MOD_NAME = "Iguana Tweaks";
     public static final String VERSION = "@MOD_VERSION@";
-    public static final String DEPENDENCIES = "required-after:Forge@[10.12.2.1121,);";
+    public static final String DEPENDENCIES = "required-after:Forge@[10.13.0.1185,);";
     public static final boolean IS_RELEASE = Boolean.parseBoolean("@IS_RELEASE@");
 }
